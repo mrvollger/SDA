3f49a961547c661740c6825cb43f5b831f569a1f
mvollger <mrvollger@gmail.com>
2018-10-08 08:30:09 -0700

    Modifying the run script to work with the new mem, and cores parameters in the snakemake.

--- ProcessCollapsedAssembly.snake.sh
+++ ProcessCollapsedAssembly.snake.sh
@@ -12,24 +12,25 @@ logDir=logs
 mkdir -p $logDir
 E=$logDir'/snakejob_{rule}_{wildcards}_e'
 O=$logDir'/snakejob_{rule}_{wildcards}_o'
-ram=4G
-defaultCores=1
 
 # run snakemake
 # the first set of line runs it on a sub grid cluster
-if [ "sungrid" == "no" ]; then 
+if [ "sungrid" == "sungrid" ]; then 
 	snakemake -p \
 		-s $snakefile \
 		--drmaa " -P eichlerlab \
 			-q eichler-short.q \
 			-l h_rt=24:00:00  \
-			-l mfree=$ram \
 			-V -cwd -e $E -o $O \
-			{params.cluster} \
+			-l mfree={params.mem} \
+			-pe serial {params.cores} \
 			-S /bin/bash" \
 		--jobs $jobNum \
 		--latency-wait $waitTime \
 		$1 $2 $3  # just a way to pass aditional arguments to snakemake, like --unlock 
+
+# params.{cores,mem} goes and looks into the snakemake for the mem and cluster params and passes them as additional arguments
+
 else
 	snakemake -p \
 		-s $snakefile \
